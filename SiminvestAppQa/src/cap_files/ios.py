d74d737f6e7d68d94ca52966f87f68d3a45b51f1
manish <manishsaini74.ms@gmail.com>
2021-11-16 14:27:16 +0530

    Test Case Added SQ-145

---
+++ SiminvestAppQa/src/cap_files/ios.py
@@ -0,0 +1,8 @@
+cp = {
+  "platformName": "iOS",
+  "platformVerison": "15.0.2",
+  "udid": "00008030-000235891451402E",
+  "automationName": "XCUITest",
+  "deviceName": "Manish Saini’s iPhone",
+  "bundleId": "com.simas.siminvest"
+}
