9b041fed1572745ce7e8f1aac874384d5432e204
Pawan Kumar <pawan@binmile.com>
2021-11-29 11:48:34 +0530

    SQ-194 Add test cases SMMA_002

--- SiminvestAppQa/test/Android/test_user_login.py
+++ SiminvestAppQa/test/Android/test_user_login.py
@@ -5,6 +5,7 @@ from SiminvestAppQa.src.pages.Android_pages.home_page import HomePage
 LoginPage = LoginPage()
 HomePage = HomePage()
 
+#Test cases for login with valid new number
 @pytest.mark.SMMA_001
 @pytest.mark.Android
 def test_login_with_valid_mobile_no():
@@ -17,7 +18,16 @@ def test_login_with_valid_mobile_no():
     LoginPage.click_agresif_profile()
     LoginPage.verify_home_page()
 
+#Test case for login with reg Number
 @pytest.mark.SMMA_002
+@pytest.mark.Android
+def test_login_with_invalid_no_less_then_10():
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no("808343")
+    LoginPage.verify_selanjutnya()
+
+#Test case for
+@pytest.mark.SMMA_003
 @pytest.mark.Android
 def test_login_with_valid_reg_mobile_no():
     LoginPage.click_mulai_sekarang()
