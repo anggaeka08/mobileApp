b7be6c0552c1c9d45013a549b04438870856a91b
Pawan Kumar <pawan@binmile.com>
2021-12-15 13:54:33 +0530

    SQ-232 Add test case for again otp

--- SiminvestAppQa/test/Android/test_user_login.py
+++ SiminvestAppQa/test/Android/test_user_login.py
@@ -1,6 +1,7 @@
 import pytest
 from SiminvestAppQa.src.pages.Android_pages.login_page import LoginPage
 from SiminvestAppQa.src.pages.Android_pages.home_page import HomePage
+import time
 
 LoginPage = LoginPage()
 HomePage = HomePage()
@@ -36,3 +37,13 @@ def test_login_with_valid_reg_mobile_no():
     LoginPage.enter_otp("1234")
     LoginPage.enter_pin()
     LoginPage.verify_home_page_reg_user()
+
+#Test case for received again otp
+@pytest.mark.SMMA_009
+@pytest.mark.Android
+def test_did_not_receive_the_otp():
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no("8445557108")
+    LoginPage.click_selanjutnya()
+    time.sleep(35)
+    LoginPage.click_Kirim_Ulang()
