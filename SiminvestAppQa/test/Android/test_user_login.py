018cbc0ca6c5e220d5c16143029a562a9d63a88d
Pawan Kumar <pawan@binmile.com>
2022-01-03 17:59:41 +0530

    SQ-269 edit test cases after adding data file

--- SiminvestAppQa/test/Android/test_user_login.py
+++ SiminvestAppQa/test/Android/test_user_login.py
@@ -2,6 +2,7 @@ import pytest
 from SiminvestAppQa.src.pages.Android_pages.login_page import LoginPage
 from SiminvestAppQa.src.pages.Android_pages.home_page import HomePage
 from SiminvestAppQa.src.utilities.genericUtilities import generate_random_integer
+from SiminvestAppQa.src.data.userData import user_data
 import time
 
 LoginPage = LoginPage()
@@ -14,8 +15,8 @@ def test_login_with_valid_mobile_no():
     LoginPage.click_mulai_sekarang()
     LoginPage.type_mobile_no(generate_random_integer(length=7, prefix='844'))
     LoginPage.click_selanjutnya()
-    LoginPage.enter_otp("1234")
-    LoginPage.set_pin("123456")
+    LoginPage.enter_otp(user_data['valid_otp'])
+    LoginPage.set_pin(user_data['setup_pin_value'])
     LoginPage.verify_risk_profile_page()
     LoginPage.click_agresif_profile()
     #LoginPage.close_home_page_banner()
@@ -27,7 +28,7 @@ def test_login_with_valid_mobile_no():
 def test_login_with_invalid_no_less_then_10():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("808343")
+    LoginPage.type_mobile_no(user_data['wrong_length_no'])
     LoginPage.verify_selanjutnya()
 
 #Test case for
@@ -36,9 +37,9 @@ def test_login_with_invalid_no_less_then_10():
 def test_login_with_valid_reg_mobile_no():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("8445557108")
+    LoginPage.type_mobile_no(user_data['reg_no'])
     LoginPage.click_selanjutnya()
-    LoginPage.enter_otp("1234")
+    LoginPage.enter_otp(user_data['valid_otp'])
     LoginPage.enter_pin()
     #LoginPage.close_home_page_banner()
     LoginPage.verify_home_page_reg_user()
@@ -49,7 +50,7 @@ def test_login_with_valid_reg_mobile_no():
 def test_did_not_receive_the_otp():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("8445557108")
+    LoginPage.type_mobile_no(user_data['reg_no'])
     LoginPage.click_selanjutnya()
     time.sleep(35)
     LoginPage.click_Kirim_Ulang()
@@ -60,13 +61,13 @@ def test_did_not_receive_the_otp():
 def test_re_enter_phone_no_after_otp():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("8445557108")
+    LoginPage.type_mobile_no(user_data['reg_no'])
     LoginPage.click_selanjutnya()
-    LoginPage.verify_otp_page_with_phone_no("8445557108")
+    LoginPage.verify_otp_page_with_phone_no(user_data['reg_no'])
     LoginPage.click_back_button_otp_page()
-    LoginPage.type_mobile_no("8445557190")
+    LoginPage.type_mobile_no(user_data['unkyc_reg_no'])
     LoginPage.click_selanjutnya()
-    LoginPage.verify_otp_page_with_phone_no("8445557190")
+    LoginPage.verify_otp_page_with_phone_no(user_data['unkyc_reg_no'])
 
 #Test case for check_login_with_reset_code_functionality after pin error
 @pytest.mark.SMMA_013
@@ -74,17 +75,17 @@ def test_re_enter_phone_no_after_otp():
 def test_check_login_with_reset_code_functionality_after_pin_error():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("8445557108")
+    LoginPage.type_mobile_no(user_data['reg_no'])
     LoginPage.click_selanjutnya()
-    LoginPage.enter_otp("1234")
+    LoginPage.enter_otp(user_data['valid_otp'])
     LoginPage.enter_wrong_pin()
     LoginPage.verify_wrong_pin_message()
     LoginPage.click_on_reset_pin()
-    LoginPage.enter_otp("1234")
+    LoginPage.enter_otp(user_data['valid_otp'])
     time.sleep(2)
-    LoginPage.set_pin("123456")
+    LoginPage.set_pin(user_data['setup_pin_value'])
     time.sleep(3)
-    LoginPage.set_pin("123456")
+    LoginPage.set_pin(user_data['setup_pin_value'])
     time.sleep(2)
     LoginPage.confirm_pin_reset()
     time.sleep(10)
@@ -96,9 +97,9 @@ def test_check_login_with_reset_code_functionality_after_pin_error():
 def test_check_valid_input_security_code_flow():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("8445557108")
+    LoginPage.type_mobile_no(user_data['reg_no'])
     LoginPage.click_selanjutnya()
-    LoginPage.enter_otp("1234")
+    LoginPage.enter_otp(user_data['valid_otp'])
     LoginPage.enter_pin()
     #LoginPage.close_home_page_banner()
     LoginPage.verify_home_page_reg_user()
@@ -109,15 +110,15 @@ def test_check_valid_input_security_code_flow():
 def test_check_reset_pin_funtionality_without_failed_login():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("8445557108")
+    LoginPage.type_mobile_no(user_data['reg_no'])
     LoginPage.click_selanjutnya()
-    LoginPage.enter_otp("1234")
+    LoginPage.enter_otp(user_data['valid_otp'])
     LoginPage.click_on_reset_pin()
-    LoginPage.enter_otp("1234")
+    LoginPage.enter_otp(user_data['valid_otp'])
     time.sleep(2)
-    LoginPage.set_pin("123456")
+    LoginPage.set_pin(user_data['setup_pin_value'])
     time.sleep(3)
-    LoginPage.set_pin("123456")
+    LoginPage.set_pin(user_data['setup_pin_value'])
     time.sleep(2)
     LoginPage.confirm_pin_reset()
     time.sleep(10)
@@ -129,9 +130,9 @@ def test_check_reset_pin_funtionality_without_failed_login():
 def test_check_invalid_input_security_code_flow():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("8445557108")
+    LoginPage.type_mobile_no(user_data['reg_no'])
     LoginPage.click_selanjutnya()
-    LoginPage.enter_otp("1234")
+    LoginPage.enter_otp(user_data['valid_otp'])
     LoginPage.enter_wrong_pin()
     time.sleep(2)
     LoginPage.verify_wrong_pin_message()
@@ -143,9 +144,9 @@ def test_check_invalid_input_security_code_flow():
 def test_security_code_check_after_timeout_session():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("8445557108")
+    LoginPage.type_mobile_no(user_data['reg_no'])
     LoginPage.click_selanjutnya()
-    LoginPage.enter_otp("1234")
+    LoginPage.enter_otp(user_data['valid_otp'])
     LoginPage.enter_pin()
     #LoginPage.close_home_page_banner()
     LoginPage.verify_home_page_reg_user()
@@ -161,9 +162,9 @@ def test_security_code_check_after_timeout_session():
 def test_logout_feature_from_security_code_page():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
-    LoginPage.type_mobile_no("8445557108")
+    LoginPage.type_mobile_no(user_data['reg_no'])
     LoginPage.click_selanjutnya()
-    LoginPage.enter_otp("1234")
+    LoginPage.enter_otp(user_data['valid_otp'])
     LoginPage.click_on_enterPin_logout_button()
     LoginPage.verify_starting_page()
 
@@ -175,8 +176,8 @@ def test_enable_and_disable_finger_print():
     LoginPage.click_mulai_sekarang()
     LoginPage.type_mobile_no(generate_random_integer(length=7, prefix='844'))
     LoginPage.click_selanjutnya()
-    LoginPage.enter_otp("1234")
-    LoginPage.set_pin(123456)
+    LoginPage.enter_otp(user_data['valid_otp'])
+    LoginPage.set_pin(user_data['setup_pin_value'])
     LoginPage.verify_risk_profile_page()
     LoginPage.click_agresif_profile()
     #LoginPage.close_home_page_banner()
