cd780885594c6ac014c17c2f7d5a72125efa7bc2
Pawan Kumar <pawan@binmile.com>
2022-01-07 10:25:39 +0530

    SQ-277 Add test cases for login according to regression suite

--- SiminvestAppQa/src/data/userData.py
+++ SiminvestAppQa/src/data/userData.py
@@ -3,5 +3,6 @@ user_data = {
     'valid_otp':'1234',
     'setup_pin_value':'123456',
     'wrong_length_no':'808343',
-    'unkyc_reg_no':'8445557190'
+    'unkyc_reg_no':'8445557190',
+    'max_lenth_no':'844123456789123'
 }
--- SiminvestAppQa/test/Android/test_user_login_logout.py
+++ SiminvestAppQa/test/Android/test_user_login_logout.py
@@ -84,7 +84,7 @@ def test_verify_redirect_to_welcome_page_after_logout_click():
 #Validate the first time login user should be redirected to type of risk profile selection page when user enter 6 digit pin
 @pytest.mark.SMMA_010
 @pytest.mark.Android
-def verify_redirect_to_risk_profile_page_after_enter_pin_for_new_no():
+def test_verify_redirect_to_risk_profile_page_after_enter_pin_for_new_no():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
     LoginPage.type_mobile_no(generate_random_integer(length=7, prefix='844'))
@@ -96,7 +96,7 @@ def verify_redirect_to_risk_profile_page_after_enter_pin_for_new_no():
 # Validate user should redirected to home page when user login with same account 2nd time and in future.
 @pytest.mark.SMMA_011
 @pytest.mark.Android
-def verify_redirect_to_home_when_login_with_same_no_second_time():
+def test_verify_redirect_to_home_when_login_with_same_no_second_time():
     number = generate_random_integer(length=7, prefix='844')
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
@@ -114,13 +114,126 @@ def verify_redirect_to_home_when_login_with_same_no_second_time():
     LoginPage.click_selanjutnya()
     LoginPage.enter_otp(user_data['valid_otp'])
     LoginPage.enter_pin()
-    LoginPage.verify_home_page_reg_user()
+    LoginPage.verify_home_page()
 
 # Validate login in with invalid mobile number
 @pytest.mark.SMMA_012
 @pytest.mark.Android
-def validate_login_with_invalid_mobile_number():
+def test_validate_login_with_invalid_mobile_number():
     LoginPage.launch_app_again()
     LoginPage.click_mulai_sekarang()
     LoginPage.type_mobile_no(user_data['wrong_length_no'])
     LoginPage.verify_selanjutnya()
+
+# Validate enter mobile number page should accept maximum 12 digit number
+@pytest.mark.SMMA_013
+@pytest.mark.Android
+def test_validate_entry_of_max_12_digit_no_in_phone_section():
+    LoginPage.launch_app_again()
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no(user_data['max_lenth_no'])
+    LoginPage.verify_count_of_mobile_no(user_data['max_lenth_no'])
+
+# Validate login with invalid PIN
+@pytest.mark.SMMA_015
+@pytest.mark.Android
+def test_validate_login_with_invalid_phone():
+    LoginPage.launch_app_again()
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no(user_data['reg_no'])
+    LoginPage.click_selanjutnya()
+    LoginPage.enter_otp(user_data['valid_otp'])
+    LoginPage.enter_wrong_pin()
+    LoginPage.verify_wrong_pin_message()
+
+# Validate reset pin functionality is working fine
+@pytest.mark.SMMA_016
+@pytest.mark.Android
+def test_validate_reset_pin_functinality():
+    LoginPage.launch_app_again()
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no(user_data['reg_no'])
+    LoginPage.click_selanjutnya()
+    LoginPage.enter_otp(user_data['valid_otp'])
+    LoginPage.click_on_reset_pin()
+    LoginPage.enter_otp(user_data['valid_otp'])
+    time.sleep(2)
+    LoginPage.set_pin(user_data['setup_pin_value'])
+    time.sleep(3)
+    LoginPage.set_pin(user_data['setup_pin_value'])
+    time.sleep(2)
+    LoginPage.confirm_pin_reset()
+    time.sleep(10)
+    LoginPage.verify_redirect_to_pin_page()
+    LoginPage.enter_pin()
+    LoginPage.verify_home_page_reg_user()
+
+# Validate user is able to logout from the enter pin page.
+@pytest.mark.SMMA_017
+@pytest.mark.Android
+def test_validate_user_able_to_logout_from_enter_pin_page():
+    LoginPage.launch_app_again()
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no(user_data['reg_no'])
+    LoginPage.click_selanjutnya()
+    LoginPage.enter_otp(user_data['valid_otp'])
+    LoginPage.click_on_enterPin_logout_button()
+    LoginPage.verify_starting_page()
+
+# validate the back button is working fine at enter OTP page
+@pytest.mark.SMMA_018
+@pytest.mark.Android
+def test_validate_back_btn_at_enter_otp_page():
+    LoginPage.launch_app_again()
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no(user_data['reg_no'])
+    LoginPage.click_selanjutnya()
+    LoginPage.verify_otp_page_with_phone_no(user_data['reg_no'])
+    LoginPage.click_back_button_otp_page()
+    LoginPage.verify_mobile_no_page()
+
+# Validate user is able to login after user click on resend OTP page
+@pytest.mark.SMMA_021
+@pytest.mark.Android
+def test_validate_login_after_resend_otp():
+    LoginPage.launch_app_again()
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no(user_data['reg_no'])
+    LoginPage.click_selanjutnya()
+    time.sleep(35)
+    LoginPage.click_Kirim_Ulang()
+    LoginPage.enter_otp(user_data['valid_otp'])
+    LoginPage.enter_pin()
+    LoginPage.verify_home_page_reg_user()
+
+# validate the timeout of OTP page is running even after user switch the application while timeout is running.
+@pytest.mark.SMMA_022
+@pytest.mark.Android
+def test_validate_otp_timout_timmer_while_app_in_backgroud():
+    LoginPage.launch_app_again()
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no(user_data['reg_no'])
+    LoginPage.click_selanjutnya()
+    LoginPage.verify_otp_timmer()
+
+# Validate reset functionality is working fine on login page.
+@pytest.mark.SMMA_023
+@pytest.mark.Android
+def test_validate_reset_functionality_working_fine_on_login_page():
+    LoginPage.launch_app_again()
+    LoginPage.click_mulai_sekarang()
+    LoginPage.type_mobile_no(user_data['reg_no'])
+    LoginPage.click_selanjutnya()
+    LoginPage.enter_otp(user_data['valid_otp'])
+    LoginPage.click_on_reset_pin()
+    LoginPage.enter_otp(user_data['valid_otp'])
+    time.sleep(2)
+    LoginPage.set_pin(user_data['setup_pin_value'])
+    time.sleep(3)
+    LoginPage.set_pin(user_data['setup_pin_value'])
+    time.sleep(2)
+    LoginPage.confirm_pin_reset()
+    time.sleep(10)
+    LoginPage.verify_redirect_to_pin_page()
+    LoginPage.enter_pin()
+    LoginPage.verify_home_page_reg_user()
