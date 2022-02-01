08ac5e3c5c0fac6f39cf779481cbac96e43896e4
Pawan Kumar <pawan@binmile.com>
2022-02-01 13:57:19 +0530

    SQ-335 Add emilutor cap file

---
+++ SiminvestAppQa/src/cap_files/emulator.py
@@ -0,0 +1,8 @@
+cp = {
+  "appPackage": "com.simas.siminvest",
+  "appActivity": "com.simas.siminvest.MainActivity",
+  "platformName": "Android",
+  "platformVersion": "10",
+  "deviceName": "Pixel3XL",
+  "udid": "emulator-5554"
+}
