069d81d41fa21d7dbb90ae83563272c06ffc07c8
Pawan Kumar <pawan@binmile.com>
2022-02-25 15:09:39 +0530

    SQ-392 add test case for amend smma_006, smma_009

--- SiminvestAppQa/test/Android/test_amend_process.py
+++ SiminvestAppQa/test/Android/test_amend_process.py
@@ -1,9 +1,8 @@
 import pytest
 from SiminvestAppQa.src.pages.Android_pages.amend_page import AmendProcess
-from SiminvestAppQa.src.pages.Android_pages.buy_process import BuyProcess
 from SiminvestAppQa.src.data.userData import user_data
 
-class Amend_test(AmendProcess,BuyProcess):
+class Amend_test(AmendProcess):
 
     #Validate user is able to increase and decrease the value of beli di harga and jumlah lot inside amend page of purchased stock.
     @pytest.mark.AMD_SMMA_001
@@ -57,3 +56,25 @@ class Amend_test(AmendProcess,BuyProcess):
         self.click_amend_btn_amend_page()
         self.click_on_confirm_btn()
         self.verify_error_message_after_exchange_market()
+
+    # Validate user is able to select buy and sell value from below given QTY buy page.
+    @pytest.mark.AMD_SMMA_006
+    @pytest.mark.Amend
+    @pytest.mark.Android
+    def test_user_able_to_select_price_from_ask_bid(self):
+        self.open_trans_page_with_reg_user(user_data['reg_no'])
+        self.open_status_page_of_buy_order()
+        self.verify_order_status_page()
+        self.click_on_amend_btn()
+        self.verify_amend_purchase_page()
+        self.verify_user_able_to_set_ask_bid_value_in_harga()
+
+    #Validate if price and lot equal - no need to validate buying power at the time of amend.
+    @pytest.mark.AMD_SMMA_009
+    @pytest.mark.Amend
+    @pytest.mark.Android
+    def test_verify_price_and_lot_for_both_pages(self):
+        self.open_trans_page_with_reg_user(user_data['reg_no'])
+        self.open_status_page_of_buy_order()
+        self.verify_order_status_page()
+        self.verify_lot_harga_on_two_pages()
