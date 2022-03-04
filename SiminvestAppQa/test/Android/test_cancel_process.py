783d2cac246c53392b5ebd9bec5812f3d23cd4b1
Pawan Kumar <pawan@binmile.com>
2022-03-04 18:00:57 +0530

    SQ-411 change amrker

--- SiminvestAppQa/test/Android/test_cancel_process.py
+++ SiminvestAppQa/test/Android/test_cancel_process.py
@@ -7,7 +7,7 @@ class Cancel_test(CancelProcess, BuyProcess_test):
 
     #Validate user should redirected to order page when user click on OK button on cancel confirmation page.
     @pytest.mark.CAN_SMMA_010
-    @pytest.mark.Amend
+    @pytest.mark.Cancel
     @pytest.mark.Android
     def test_validate_positive_flow_for_cancel_feature(self):
         self.test_validate_status_on_transaction_page()
@@ -19,7 +19,7 @@ class Cancel_test(CancelProcess, BuyProcess_test):
 
     #Validate user should not allow to cancel the order outside exchange hours.
     @pytest.mark.CAN_SMMA_003
-    @pytest.mark.Amend
+    @pytest.mark.Cancel
     @pytest.mark.Android
     def test_validate_pop_message_after_outside_exchange_hour(self):
         self.open_trans_page_with_reg_user(user_data['reg_no'])
@@ -33,7 +33,7 @@ class Cancel_test(CancelProcess, BuyProcess_test):
 
     #Validate all the words are grammatically correct at the time of cancel process.
     @pytest.mark.CAN_SMMA_004
-    @pytest.mark.Amend
+    @pytest.mark.Cancel
     @pytest.mark.Android
     def test_grammatically_correct_at_the_time_of_cancel_process(self):
         self.open_trans_page_with_reg_user(user_data['reg_no'])
@@ -42,7 +42,7 @@ class Cancel_test(CancelProcess, BuyProcess_test):
 
     #Validate user is redirected to cancel and amend page when user click on tidak button on cancel confirmation page.
     @pytest.mark.CAN_SMMA_011
-    @pytest.mark.Amend
+    @pytest.mark.Cancel
     @pytest.mark.Android
     def test_validate_redirection_after_click_on_cancel(self):
         self.open_trans_page_with_reg_user(user_data['reg_no'])
