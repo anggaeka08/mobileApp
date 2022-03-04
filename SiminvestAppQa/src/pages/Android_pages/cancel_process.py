6bb522b11290fab609a8ea741e0b465892d56dfa
Pawan Kumar <pawan@binmile.com>
2022-03-04 10:40:07 +0530

    SQ-407 Add files for cancel

---
+++ SiminvestAppQa/src/pages/Android_pages/cancel_process.py
@@ -0,0 +1,8 @@
+from SiminvestAppQa.src.data.userData import user_data
+from SiminvestAppQa.src.pages.Android_pages.amend_page import AmendProcess
+import time
+import allure
+import logging as logger
+
+class CancelProcess(AmendProcess):
+    pass
---
+++ SiminvestAppQa/test/Android/test_cancel_process.py
@@ -0,0 +1,6 @@
+import pytest
+from SiminvestAppQa.src.pages.Android_pages.cancel_process import CancelProcess
+from SiminvestAppQa.src.data.userData import user_data
+
+class Cancel_test(CancelProcess):
+    pass
