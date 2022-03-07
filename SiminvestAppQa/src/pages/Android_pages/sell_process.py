93c306bd4a6a4b27e420aa404e96a4a4c52ce3dd
Pawan Kumar <pawan@binmile.com>
2022-03-07 15:22:54 +0530

    SQ-415 add test case smma_502 for sell

---
+++ SiminvestAppQa/src/pages/Android_pages/sell_process.py
@@ -0,0 +1,34 @@
+from SiminvestAppQa.src.data.userData import user_data
+from SiminvestAppQa.src.pages.Android_pages.home_page import HomePage
+import allure
+
+jaul_btn = '//*[@text="Jual"]'
+Setuju_btn = '//*[@text="Setuju"]'
+bs_on_trans = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup[1]/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[5]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView[4]'
+
+
+class SellProcess(HomePage):
+
+    @allure.step("ope and verify portfolio page")
+    def open_and_verify_portfolio(self, number):
+        self.click_mulai_sekarang()
+        self.type_mobile_no(number)
+        self.click_selanjutnya()
+        self.enter_otp(user_data['valid_otp'])
+        self.enter_pin()
+        self.close_home_page_banner()
+        self.verify_home_page_reg_user()
+        self.click_on_portfolio_btn()
+        self.verify_portfolio_for_kyc_user()
+
+    @allure.step("Click on jual button on sdp page")
+    def click_on_jual_btn(self):
+        self.click(jaul_btn)
+
+    @allure.step("click on setuju page")
+    def click_on_setuju(self):
+        self.click(Setuju_btn)
+
+    @allure.step("Verify transaction for sell")
+    def verify_transaction_page_for_sell(self):
+        self.assert_equal(self.get_attribute(bs_on_trans, "text"), 'SELL')
