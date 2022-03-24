bd92ec1fcd073ffbb53c2d2d6a4fd2187e69b678
Pawan Kumar <pawan@binmile.com>
2022-03-24 15:07:17 +0530

    Add cap file

---
+++ SiminvestAppQa/src/cap_files/bs_android_cap.py
@@ -0,0 +1,43 @@
+cp =[{
+    # Set your access credentials
+    "browserstack.user": "pawan_N1RjdY",
+    "browserstack.key": "bz4L2FFH1bYoQ4N58HTN",
+
+    # Set URL of the application under test
+    "app": "bs://3c6959f2eeb765e00ad7a683fd6da1ce2bd1990f",
+
+    # Specify device and os_version for testing
+    "device": "Xiaomi Redmi Note 7",
+    "os_version": "9.0",
+
+    # Set other BrowserStack capabilities
+    "project": "Siminvest APP QA",
+    "build": "Android 1.49.6 All Test Cases",
+    "name": "UI_Test Android_1",
+
+    # Set Specific capabilities regarding test
+    'autoAcceptAlerts': 'true',
+    #"browserstack.local": "true"
+},
+
+{
+    # Set your access credentials
+    "browserstack.user": "pawan_N1RjdY",
+    "browserstack.key": "bz4L2FFH1bYoQ4N58HTN",
+
+    # Set URL of the application under test
+    "app": "bs://3c6959f2eeb765e00ad7a683fd6da1ce2bd1990f",
+
+    # Specify device and os_version for testing
+    "device": "Xiaomi Redmi Note 9",
+    "os_version": "10.0",
+
+    # Set other BrowserStack capabilities
+    "project": "Siminvest APP QA",
+    "build": "Android 1.49.6 All Test Cases",
+    "name": "UI_Test Android_2",
+
+    # Set Specific capabilities regarding test
+    'autoAcceptAlerts': 'true',
+    #"browserstack.local": "true"
+}]
