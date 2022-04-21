1ee3b203d100e60e618e22e4777dd3f4e764122e
Pawan Kumar <pawan@binmile.com>
2022-04-21 17:31:27 +0530

    SQ-525 Add test case for SDP smma_003_004

--- SiminvestAppQa/test/Android/test_stock_details_page.py
+++ SiminvestAppQa/test/Android/test_stock_details_page.py
@@ -28,3 +28,19 @@ class SDP_test(StockDetailPage, BuyProcess):
         self.go_back()
         self.scroll_ups()
         self.verify_stock_on_watchlist(False)
+
+    # Validate search option on sdp page
+    @pytest.mark.SDP_SMMA_003_004
+    @pytest.mark.Android
+    @pytest.mark.SDP
+    def test_validate_star_mark_on_sdp(self):
+        self.open_sdp_page_with_kyc_user(user_data['reg_no'], 'ACES')
+        self.click_on_search_btn()
+        self.enter_stock_code('ACES')
+        self.click_on_stocks()
+        self.verify_sdp_page()
+        self.go_back()
+        self.go_back()
+        self.verify_home_page_reg_user()
+
+
