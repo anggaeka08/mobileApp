d82d73743f9b81f621281963b66f7b8d1ca9dd97
Pawan Kumar <pawan@binmile.com>
2022-04-21 14:10:50 +0530

    SQ-521 Add test case for sdp smma_001_002

---
+++ SiminvestAppQa/test/Android/test_stock_details_page.py
@@ -0,0 +1,30 @@
+import pytest
+from SiminvestAppQa.src.pages.Android_pages.stock_detail_page import StockDetailPage
+from SiminvestAppQa.src.pages.Android_pages.buy_process import BuyProcess
+from SiminvestAppQa.src.data.userData import user_data
+
+
+@pytest.mark.usefixtures("unittest_setUpClass_fixture_SDP_test")
+class SDP_test(StockDetailPage, BuyProcess):
+
+    # Validate user is able to star mark the stock/ Validate user is able to un-mark the star.
+    @pytest.mark.SDP_SMMA_001_002
+    @pytest.mark.Android
+    @pytest.mark.SDP
+    def test_validate_star_mark_on_sdp(self):
+        self.open_sdp_page_with_kyc_user(user_data['reg_no'], 'ACES')
+        self.scroll_down()
+        self.tap_on_star()
+        self.click_on_watchlist()
+        self.sleep(2)
+        self.go_back()
+        self.go_back()
+        self.sleep(2)
+        self.scroll_up()
+        self.verify_stock_on_watchlist(True)
+        self.click_on_stock()
+        self.tap_on_star()
+        self.click_on_watchlist()
+        self.go_back()
+        self.scroll_ups()
+        self.verify_stock_on_watchlist(False)
