f3b53613cdd5e3f07199ea8c08894d4f395f202c
Pawan Kumar <pawan@binmile.com>
2022-05-06 16:30:24 +0530

    SQ-559 add test case smma_006_007_008 for portfolio

--- SiminvestAppQa/test/Android/test_portfolio.py
+++ SiminvestAppQa/test/Android/test_portfolio.py
@@ -19,3 +19,15 @@ class Portfolio_test(Portfolio):
         self.verify_saham_tab_clickable()
         self.verify_reksadhana_tab_clickable()
 
+    # Cover all 3 test cases in single test
+    @pytest.mark.Port_SMMA_006_007_008
+    @pytest.mark.Android
+    @pytest.mark.portfolio
+    def test_validate_presence_of_values_and(self):
+        self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
+        self.click_on_portfolio_btn()
+        self.verify_portfolio_for_kyc_user()
+        self.verify_buypower_and_other_values_presence()
+        self.verify_presence_of_Code_Lot_Avg_Last_PL_IDR_PL_percentage_invested_value()
+        self.validate_stock_row_availability_in_portfolio()
+
