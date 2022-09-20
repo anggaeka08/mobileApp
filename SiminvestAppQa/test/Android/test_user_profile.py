a79876a6258c2bd0436f72b739e6a09d29b59303
Pawan Kumar <pawan@binmile.com>
2022-09-20 21:05:08 +0530

    SQ-1085 Update test cases

--- SiminvestAppQa/test/Android/test_user_profile.py
+++ SiminvestAppQa/test/Android/test_user_profile.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 from SiminvestAppQa.src.data.userData import user_data
 from SiminvestAppQa.src.pages.Android_pages.user_profile import UserProfile
@@ -10,6 +11,7 @@ class userProfile_test(UserProfile):
     @pytest.mark.User_profile_RateFeature_01
     @pytest.mark.android
     @pytest.mark.userProfile
+    @allure.story("F-3:Profile Feature")
     def test_validate_rate_feature_all_testcases(self):
         number = generate_random_integer(length=7, prefix='844')
         self.login_with_new_number(number)
@@ -38,6 +40,7 @@ class userProfile_test(UserProfile):
     @pytest.mark.User_profile_RateFeature_02
     @pytest.mark.android
     @pytest.mark.userProfile
+    @allure.story("F-3:Profile Feature")
     def test_validate_on_two_start_rating(self):
         number = generate_random_integer(length=6, prefix='8442')
         self.login_with_new_number(number)
@@ -53,6 +56,7 @@ class userProfile_test(UserProfile):
     @pytest.mark.User_profile_RateFeature_03
     @pytest.mark.android
     @pytest.mark.userProfile
+    @allure.story("F-3:Profile Feature")
     def test_validate_submit_feedback_by_default_keyboard_btn(self):
         number = generate_random_integer(length=6, prefix='8442')
         self.login_with_new_number(number)
@@ -67,6 +71,8 @@ class userProfile_test(UserProfile):
     @pytest.mark.User_profile_otherPages_01
     @pytest.mark.android
     @pytest.mark.userProfile
+    @pytest.mark.otherFeature
+    @allure.story("F-3:Profile Feature")
     def test_validate_ajak_btn_and_refferal_feature(self):
         self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
         self.click_on_profile_btn()
@@ -88,6 +94,8 @@ class userProfile_test(UserProfile):
     @pytest.mark.User_profile_otherPages_02
     @pytest.mark.android
     @pytest.mark.userProfile
+    @pytest.mark.otherFeature
+    @allure.story("F-3:Profile Feature")
     def test_validate_profile_page_for_non_kyc_user(self):
         self.login_with_non_kyc_number(user_data['unkyc_reg_no'])
         self.click_on_profile_btn()
@@ -103,6 +111,8 @@ class userProfile_test(UserProfile):
     @pytest.mark.User_profile_otherPages_03
     @pytest.mark.android
     @pytest.mark.userProfile
+    @pytest.mark.otherFeature
+    @allure.story("F-3:Profile Feature")
     def test_validate_user_profile_image_upload_option(self):
         self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
         self.click_on_profile_btn()
@@ -114,7 +124,6 @@ class userProfile_test(UserProfile):
         self.verify_image_icon_availability()
         self.click_on_profile_icon()
         self.upload_image_by_camera_option()
-        self.verify_image_icon_availability()
         self.click_on_profile_icon()
         self.uplaod_cancel_process()
         self.verify_image_icon_availability()
@@ -122,6 +131,8 @@ class userProfile_test(UserProfile):
     @pytest.mark.User_profile_otherPages_04
     @pytest.mark.android
     @pytest.mark.userProfile
+    @pytest.mark.otherFeature
+    @allure.story("F-3:Profile Feature")
     def test_validate_user_profile_image_upload_by_gallery(self):
         self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
         self.click_on_profile_btn()
