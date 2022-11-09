810e95260eee40f486abb48119b59f82d9601372
Pawan Kumar <pawan@binmile.com>
2022-11-09 13:03:07 +0530

    SQ-1174 Lambda test setup

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -1,21 +1,25 @@
 cp = [
 {
-    "app": "lt://APP10160501221660619630949854",
+    "app": "lt://APP10160532421667975830302173",
     "platformName": "Android",
     "deviceName": "Google Pixel 4",
     "platformVersion": "10",
     "build": "Build 1.60.8 parallel",
     "name": "Login Test Cases",
-    "isRealMobile": True
+    "isRealMobile": True,
+    "user": "pawan@binmile.com",
+    "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
     },
 {
-    "app": "lt://APP10160501221660619630949854",
+    "app": "lt://APP10160532421667975830302173",
     "platformName": "Android",
     "deviceName": "Google Pixel 3",
     "platformVersion": "9",
     "build": "Build 1.60.8 Login Demo",
     "name": "Login Test Cases",
-    "isRealMobile": True
+    "isRealMobile": True,
+    "user": "pawan@binmile.com",
+    "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
     }
 
 ]
