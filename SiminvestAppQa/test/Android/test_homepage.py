74e97d6a02f552e75b4124a5c1a8ebed838ef3f9
Pawan Kumar <pawan@binmile.com>
2022-11-15 16:31:38 +0530

    SQ-1176 Add test cases for all btns on homepage

--- SiminvestAppQa/test/Android/test_homepage.py
+++ SiminvestAppQa/test/Android/test_homepage.py
@@ -9,7 +9,7 @@ import time
 @pytest.mark.usefixtures("_unittest_setUpClass_fixture_homePage_test")
 class homePage_test(HomePage,LoginPage):
 
-    # Validate user is able open the SDP page via global search bar.
+    # Validate global search bar.
     @pytest.mark.GlobalSaerchOption
     @pytest.mark.Homepage
     @pytest.mark.Android
@@ -38,6 +38,75 @@ class homePage_test(HomePage,LoginPage):
         self.click_global_search_btn_and_saerch_stock('Simas Saham Bertumbuh')
         self.sleep(3)
         self.validate_MF_search_in_global_search_and_Redirection_after_click()
+
+    # Validate all buttons on homepage.
+    @pytest.mark.AllButtons
+    @pytest.mark.Homepage
+    @pytest.mark.Android
+    @allure.story("F-5:HomePage Feature")
+    def test_validate_all_buttons_on_homepage(self):
+        self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
+        self.click_to_soldo_rdn()
+        self.verify_rdn_balance_page()
+        self.click_on_top_up_btn()
+        self.verify_topup_page()
+        self.go_back()
+        self.click_on_tarik_dana_btn()
+        self.verify_tarik_dana_page()
+        self.go_back()
+        self.click_on_riwayat_btn()
+        self.verify_riwayat_page()
+        self.go_back()
+        self.go_back()
+        self.verify_star_point_btn()
+        self.go_back()
+        self.verify_Reksadana_page()
+        self.click_top_up_from_homepage()
+        self.verify_topup_page()
+        self.go_back()
+        self.click_on_indeks_btn()
+        self.verify_indeks_page()
+        self.go_back()
+        self.click_on_sector_button()
+        self.verify_sector_page()
+        self.go_back()
+        self.click_on_event_btn()
+        self.verify_event_page()
+        self.go_back()
+        self.scroll_up()
+        self.click_on_eipo_btn()
+        self.verify_eipo_page()
+        self.verify_eipo_entry()
+        self.go_back()
+        self.verify_top_frequency_presention()
+        self.verify_stock_presence_in_top_frequency()
+        self.click_on_TF_down_arrow()
+        self.verify_half_card_page()
+        self.go_back()
+        self.verify_home_page_reg_user_after_back_from_watchlist_new()
+        self.click_on_see_more_btn()
+        self.verify_mover_page()
+        self.go_back()
+        self.verify_home_page_reg_user_after_back_from_watchlist_new()
+        self.scroll_up()
+        self.click_on_default_btn()
+        self.verify_watchlist_card()
+        self.verify_watchlist_details()
+        self.go_back()
+        self.verify_home_page_reg_user_after_back_from_watchlist_new()
+        self.click_stock_1_from_top_frequency_list()
+        self.sleep(3)
+        self.verify_sdp_page_from_top_freqency_list()
+        self.go_back()
+        self.verify_home_page_reg_user_after_back_from_watchlist_new()
+        self.click_on_default_watchlist_entry_1()
+        self.verify_sdp_page_from_top_freqency_list()
+        self.go_back()
+        self.verify_home_page_reg_user_after_back_from_watchlist_new()
+        self.verify_edit_btn()
+
+
+
 
 
 
