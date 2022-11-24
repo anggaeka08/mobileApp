db0b5d546c59fbc0961efb826061c40a5a4e3b76
Pawan Kumar <pawan@binmile.com>
2022-11-24 16:48:34 +0530

    SQ-1194 Edit Script for lambda test markers

--- SiminvestAppQa/test/Android/test_research_page.py
+++ SiminvestAppQa/test/Android/test_research_page.py
@@ -2,6 +2,7 @@ import allure
 import pytest
 from SiminvestAppQa.src.data.userData import user_data
 from SiminvestAppQa.src.pages.Android_pages.research_page import Research
+from selenium.common.exceptions import NoSuchElementException
 
 class Research_test(Research):
 
@@ -11,18 +12,30 @@ class Research_test(Research):
     @pytest.mark.Revamp
     @allure.story("F-4:Research Feature")
     def test_validate_research_page_redirection_and_search_option(self):
-        self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
-        self.verify_research_tab_on_homepage()
-        self.click_on_research_btn()
-        self.verify_header_of_research_page()
-        self.Verify_search_btn_on_research_page()
-        self.click_on_search_btn()
-        self.verify_redirection_on_search_page()
-        self.Validate_place_holder_in_search_option()
-        self.Verify_availability_for_last_report_and_news_on_search_option()
-        self.enter_and_verify_some_value_in_search_option()
-        self.Click_to_new_and_verify_entry()
-        self.click_to_lastreport_and_verify_entry()
+        try:
+            self.execute_script('lambda-name=test_validate_research_page_redirection_and_search_option')
+            self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
+            self.verify_research_tab_on_homepage()
+            self.click_on_research_btn()
+            self.verify_header_of_research_page()
+            self.Verify_search_btn_on_research_page()
+            self.click_on_search_btn()
+            self.verify_redirection_on_search_page()
+            self.Validate_place_holder_in_search_option()
+            self.Verify_availability_for_last_report_and_news_on_search_option()
+            self.enter_and_verify_some_value_in_search_option()
+            self.Click_to_new_and_verify_entry()
+            self.click_to_lastreport_and_verify_entry()
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_validate_research_page_redirection_and_search_option', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_validate_research_page_redirection_and_search_option', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
+
 
     @pytest.mark.RE_014_to_019
     @pytest.mark.Android
@@ -30,13 +43,24 @@ class Research_test(Research):
     @pytest.mark.Revamp
     @allure.story("F-4:Research Feature")
     def test_validate_redirection_for_news_and_media_and_search_option(self):
-        self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
-        self.click_on_research_btn()
-        self.verify_tabs_on_research_page()
-        self.click_on_news_research_tab_and_verify_entry()
-        self.click_on_media_tab_and_verify_entry()
-        self.click_on_search_btn()
-        self.enter_some_value_in_search_option_verify_entry()
+        try:
+            self.execute_script('lambda-name=test_validate_redirection_for_news_and_media_and_search_option')
+            self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
+            self.click_on_research_btn()
+            self.verify_tabs_on_research_page()
+            self.click_on_news_research_tab_and_verify_entry()
+            self.click_on_media_tab_and_verify_entry()
+            self.click_on_search_btn()
+            self.enter_some_value_in_search_option_verify_entry()
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_validate_research_page_redirection_and_search_option', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_validate_research_page_redirection_and_search_option', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
 
     @pytest.mark.RE_022_to_039
     @pytest.mark.Android
@@ -44,24 +68,35 @@ class Research_test(Research):
     @pytest.mark.Revamp
     @allure.story("F-4:Research Feature")
     def test_validate_red_dots_on_stock_signal(self):
-        self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
-        self.click_on_research_btn()
-        self.verify_10_entries_on_news_section()
-        #self.click_to_lastreport_and_verify_entry()
-        self.click_to_daily_search_verify_redirection()
-        self.go_back()
-        self.click_on_stock_signal()
-        self.verify_red_dont_on_one_entry()
-        self.Click_on_entry_and_verify_red_dot_remove_from_entry()
-        self.close()
-        self.launch_app_again()
-        self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
-        self.click_on_research_btn()
-        self.verify_red_dont_on_one_entry_after_read()
-        self.click_to_signal_tadai_and_verify_red_dots()
-        self.go_back()
-        self.go_back()
-        self.Verify_exit_from_app_on_research_page()
+        try:
+            self.execute_script('lambda-name=test_validate_red_dots_on_stock_signal')
+            self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
+            self.click_on_research_btn()
+            self.verify_10_entries_on_news_section()
+            #self.click_to_lastreport_and_verify_entry()
+            self.click_to_daily_search_verify_redirection()
+            self.go_back()
+            self.click_on_stock_signal()
+            self.verify_red_dont_on_one_entry()
+            self.Click_on_entry_and_verify_red_dot_remove_from_entry()
+            self.close()
+            self.launch_app_again()
+            self.login_and_verify_homepage_for_reg_user(user_data['reg_no'])
+            self.click_on_research_btn()
+            self.verify_red_dont_on_one_entry_after_read()
+            self.click_to_signal_tadai_and_verify_red_dots()
+            self.go_back()
+            self.go_back()
+            self.Verify_exit_from_app_on_research_page()
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_validate_red_dots_on_stock_signal', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_validate_red_dots_on_stock_signal', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
 
 
 
--- SiminvestAppQa/test/Android/test_watchlist.py
+++ SiminvestAppQa/test/Android/test_watchlist.py
@@ -4,6 +4,7 @@ from SiminvestAppQa.src.pages.Android_pages.watchlist import Watchlist
 from SiminvestAppQa.src.utilities.genericUtilities import generate_random_string
 from SiminvestAppQa.src.data.userData import user_data
 import logging as logger
+from selenium.common.exceptions import NoSuchElementException
 
 
 class Watchlist_test(Watchlist):
@@ -15,21 +16,33 @@ class Watchlist_test(Watchlist):
     @allure.story("F-1:Watchlist Feature")
     @pytest.mark.Revamp
     def test_validate_user_able_to_add_maximum_10_watchlist(self):
-        self.go_to_watchlist_option_after_login(user_data['reg_no'])
-        self.scroll_up()
-        self.verify_stock_code_and_name_in_watchlist_stocks()
-        self.click_on_defaults_btn()
-        self.verify_watchlist_activation()
-        for i in range(10):
-            self.click_on_plus_btn()
-            self.enter_watchlist_name(f'test{i}')
-        self.validate_pop_message()
-        self.go_back()
-        self.click_on_cross_btn()
-        for i in range(9):
-            self.click_on_delete()
-            self.click_on_Hapus()
-            self.sleep(1)
+        try:
+            self.execute_script('lambda-name=test_verify_redirect_to_welcome_after_open_app')
+            self.go_to_watchlist_option_after_login(user_data['reg_no'])
+            self.scroll_up()
+            self.verify_stock_code_and_name_in_watchlist_stocks()
+            self.click_on_defaults_btn()
+            self.verify_watchlist_activation()
+            for i in range(10):
+                self.click_on_plus_btn()
+                self.enter_watchlist_name(f'test{i}')
+            self.validate_pop_message()
+            self.go_back()
+            self.click_on_cross_btn()
+            for i in range(9):
+                self.click_on_delete()
+                self.click_on_Hapus()
+                self.sleep(1)
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_verify_redirect_to_welcome_after_open_app', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_verify_redirect_to_welcome_after_open_app', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
+
 
 
     #Validate user able to create duplicate name watchlist and able to add stock
@@ -39,26 +52,37 @@ class Watchlist_test(Watchlist):
     @pytest.mark.Revamp
     @allure.story("F-1:Watchlist Feature")
     def test_user_able_to_create_duplicate_name_watchlist_and_able_to_add_stock(self):
-        self.go_to_watchlist_option_after_login(user_data['reg_no'])
-        self.click_on_defaults_btn()
-        self.click_on_plus_btn()
-        self.enter_watchlist_name('Default')
-        self.validate_watchlist_entry('Default')
-        self.click_on_watchlist_entry_2()
-        self.sleep(1)
-        self.scroll_up()
-        self.empty_watchlist_msg()
-        self.click_on_tambah_saham()
-        self.search_stock('REAL')
-        self.click_to_add_remove()
-        self.go_back()
-        self.go_back()
-        self.scroll_ups()
-        self.verify_watchlist_entry()
-        self.click_on_defaults_btn()
-        self.click_on_selected_watchlist()
-        self.click_on_Hapus()
-        self.sleep(2)
+        try:
+            self.execute_script('lambda-name=test_user_able_to_create_duplicate_name_watchlist_and_able_to_add_stock')
+            self.go_to_watchlist_option_after_login(user_data['reg_no'])
+            self.click_on_defaults_btn()
+            self.click_on_plus_btn()
+            self.enter_watchlist_name('Default')
+            self.validate_watchlist_entry('Default')
+            self.click_on_watchlist_entry_2()
+            self.sleep(1)
+            self.scroll_up()
+            self.empty_watchlist_msg()
+            self.click_on_tambah_saham()
+            self.search_stock('REAL')
+            self.click_to_add_remove()
+            self.go_back()
+            self.go_back()
+            self.scroll_ups()
+            self.verify_watchlist_entry()
+            self.click_on_defaults_btn()
+            self.click_on_selected_watchlist()
+            self.click_on_Hapus()
+            self.sleep(2)
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_user_able_to_create_duplicate_name_watchlist_and_able_to_add_stock', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_user_able_to_create_duplicate_name_watchlist_and_able_to_add_stock', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
 
     @pytest.mark.Wat_SMMA_024_028
     @pytest.mark.Android
@@ -66,20 +90,31 @@ class Watchlist_test(Watchlist):
     @pytest.mark.Revamp
     @allure.story("F-1:Watchlist Feature")
     def test_validate_selected_watchlist_visible_after_reopen_app_and_sdp_open_after_click_stock(self):
-        self.go_to_watchlist_option_after_login(user_data['reg_no'])
-        self.click_on_defaults_btn()
-        self.click_on_plus_btn()
-        self.enter_watchlist_name('test')
-        self.validate_watchlist_entry('test')
-        self.click_on_watchlist_entry_2()
-        self.app_in_background(20)
-        self.scroll_up()
-        self.click_on_test()
-        self.click_on_selected_watchlist()
-        self.click_on_Hapus()
-        self.go_back()
-        self.click_on_stock_code_in_watclist()
-        self.verify_sdp_page()
+        try:
+            self.execute_script('lambda-name=Wat_SMMA_024_028')
+            self.go_to_watchlist_option_after_login(user_data['reg_no'])
+            self.click_on_defaults_btn()
+            self.click_on_plus_btn()
+            self.enter_watchlist_name('test')
+            self.validate_watchlist_entry('test')
+            self.click_on_watchlist_entry_2()
+            self.app_in_background(20)
+            self.scroll_up()
+            self.click_on_test()
+            self.click_on_selected_watchlist()
+            self.click_on_Hapus()
+            self.go_back()
+            self.click_on_stock_code_in_watclist()
+            self.verify_sdp_page()
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('Wat_SMMA_024_028', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('Wat_SMMA_024_028', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
 
     @pytest.mark.cover_many_testcase
     @pytest.mark.Android
@@ -87,68 +122,90 @@ class Watchlist_test(Watchlist):
     @pytest.mark.Revamp
     @allure.story("F-1:Watchlist Feature")
     def test_validate_maximum_watchlist_test_cases(self):
-        self.go_to_watchlist_option_after_login(user_data['reg_no'])
-        self.scroll_up()
-        self.click_on_defaults_btn()
-        self.validate_only_default_watchlist_for_a_user()
-        self.verify_delete_btn()
-        self.go_back()
-        self.sleep(1)
-        self.scroll_up()
-        self.validate_avail_check_for_stock_on_watchlist()
-        self.click_on_defaults_btn()
-        self.click_on_plus_btn()
-        self.enter_watchlist_name('test')
-        self.validate_watchlist_entry('test')
-        self.click_on_watchlist_entry_2()
-        self.sleep(1)
-        self.scroll_up()
-        self.empty_watchlist_msg()
-        self.click_on_tambah_saham()
-        self.verify_top_gainer_presence()
-        self.click_on_top_gainer()
-        self.verify_stock_type_selection()
-        self.go_back()
-        self.go_back()
-        self.sleep(2)
-        self.small_scroll_down()
-        self.click_on_edit_for_stock()
-        self.search_stock('A')
-        self.verify_stock_list_after_single_character_search()
-        self.search_stock('ARGO')
-        self.verify_stock_search_by_full_code()
-        self.click_to_add_remove()
-        self.sleep(3)
-        self.click_to_add_remove()
-        self.click_on_ok()
-        self.go_back()
-        self.sleep(1)
-        self.scroll_up()
-        self.click_on_test()
-        self.click_on_edit_btn()
-        name = generate_random_string(65)
-        self.sleep(2)
-        self.edit_watchlist_name(name)
-        self.click_on_simpan()
-        self.validate_watchlist_entry("test"+name[:56])
-        self.Cancel_delete_and_delete_watchlist()
+        try:
+            self.execute_script('lambda-name=test_validate_maximum_watchlist_test_cases')
+            self.go_to_watchlist_option_after_login(user_data['reg_no'])
+            self.scroll_up()
+            self.click_on_defaults_btn()
+            self.validate_only_default_watchlist_for_a_user()
+            self.verify_delete_btn()
+            self.go_back()
+            self.sleep(1)
+            self.scroll_up()
+            self.validate_avail_check_for_stock_on_watchlist()
+            self.click_on_defaults_btn()
+            self.click_on_plus_btn()
+            self.enter_watchlist_name('test')
+            self.validate_watchlist_entry('test')
+            self.click_on_watchlist_entry_2()
+            self.sleep(1)
+            self.scroll_up()
+            self.empty_watchlist_msg()
+            self.click_on_tambah_saham()
+            self.verify_top_gainer_presence()
+            self.click_on_top_gainer()
+            self.verify_stock_type_selection()
+            self.go_back()
+            self.go_back()
+            self.sleep(2)
+            self.small_scroll_down()
+            self.click_on_edit_for_stock()
+            self.search_stock('A')
+            self.verify_stock_list_after_single_character_search()
+            self.search_stock('ARGO')
+            self.verify_stock_search_by_full_code()
+            self.click_to_add_remove()
+            self.sleep(3)
+            self.click_to_add_remove()
+            self.click_on_ok()
+            self.go_back()
+            self.sleep(1)
+            self.scroll_up()
+            self.click_on_test()
+            self.click_on_edit_btn()
+            name = generate_random_string(65)
+            self.sleep(2)
+            self.edit_watchlist_name(name)
+            self.click_on_simpan()
+            self.validate_watchlist_entry("test"+name[:56])
+            self.Cancel_delete_and_delete_watchlist()
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_validate_maximum_watchlist_test_cases', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_validate_maximum_watchlist_test_cases', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
 
     @pytest.mark.Wat_SMMA_013_015
     @pytest.mark.Android
     @pytest.mark.watchlist
     @pytest.mark.Revamp
     @allure.story("F-1:Watchlist Feature")
-    def test_user_not_able_to_add_more_then_15_stock_i_watchlist(self):
-        self.go_to_watchlist_option_after_login(user_data['reg_no'])
-        self.click_on_defaults_btn()
-        self.click_on_plus_btn()
-        self.enter_watchlist_name('test')
-        self.validate_watchlist_entry('test')
-        self.click_on_watchlist_entry_2()
-        self.sleep(1)
-        self.scroll_up()
-        self.click_on_tambah_saham()
-        self.add_stock_in_watchlist()
+    def test_user_not_able_to_add_more_then_15_stock_in_watchlist(self):
+        try:
+            self.execute_script('lambda-name=test_user_not_able_to_add_more_then_15_stock_in_watchlist')
+            self.go_to_watchlist_option_after_login(user_data['reg_no'])
+            self.click_on_defaults_btn()
+            self.click_on_plus_btn()
+            self.enter_watchlist_name('test')
+            self.validate_watchlist_entry('test')
+            self.click_on_watchlist_entry_2()
+            self.sleep(1)
+            self.scroll_up()
+            self.click_on_tambah_saham()
+            self.add_stock_in_watchlist()
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_user_not_able_to_add_more_then_15_stock_in_watchlist', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_user_not_able_to_add_more_then_15_stock_in_watchlist', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
 
     @pytest.mark.Wat_SMMA_25
     @pytest.mark.Android
@@ -156,25 +213,37 @@ class Watchlist_test(Watchlist):
     @pytest.mark.Revamp
     @allure.story("F-1:Watchlist Feature")
     def test_validate_same_stock_will_be_added_to_many_watchlist(self):
-        self.go_to_watchlist_option_after_login(user_data['reg_no'])
-        self.scroll_up()
-        self.click_on_defaults_btn()
-        for i in range(9):
-            self.click_on_plus_btn()
-            self.enter_watchlist_name(f'test{i}')
-        self.go_back()
-        self.add_stock_in_all_watchlist()
-        self.go_back()
-        self.sleep(1)
-        self.go_back()
-        self.sleep(1)
-        self.scroll_up()
-        self.click_on_defaults_btn()
-        self.verify_stock_in_all_watchlist()
-        for i in range(9):
-            self.click_on_delete()
-            self.click_on_Hapus()
+        try:
+            self.execute_script('lambda-name=test_validate_same_stock_will_be_added_to_many_watchlist')
+            self.go_to_watchlist_option_after_login(user_data['reg_no'])
+            self.scroll_up()
+            self.click_on_defaults_btn()
+            for i in range(9):
+                self.click_on_plus_btn()
+                self.enter_watchlist_name(f'test{i}')
+            self.go_back()
+            self.add_stock_in_all_watchlist()
+            self.go_back()
             self.sleep(1)
+            self.go_back()
+            self.sleep(1)
+            self.scroll_up()
+            self.click_on_defaults_btn()
+            self.verify_stock_in_all_watchlist()
+            for i in range(9):
+                self.click_on_delete()
+                self.click_on_Hapus()
+                self.sleep(1)
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_validate_same_stock_will_be_added_to_many_watchlist', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_validate_same_stock_will_be_added_to_many_watchlist', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
+
 
 
 
