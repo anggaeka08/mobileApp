8b2a8d75274124c01a2e2259c78e3130a62c328f
Pawan Kumar <pawan@binmile.com>
2023-01-05 14:28:36 +0530

    SQ-1246 Update app url for lambda test

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -1,21 +1,21 @@
 cp = [
 {
-    "app":"lt://APP10160621161671599743811533",
+    "app":"lt://APP10160232371672908829031496",
     "platformName": "Android",
     "deviceName": "Google Pixel 4",
     "platformVersion": "10",
-    "build": "Build 1.71.1 (305)",
-    "name": "All Test Cases",
+    "build": "Regression Build 1.72.1 (318)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
     },
 {
-    "app":"lt://APP10160621161671599743811533",
+    "app":"lt://APP10160232371672908829031496",
     "platformName": "Android",
     "deviceName": "Google Pixel 3",
     "platformVersion": "9",
-    "build": "Build 1.71.1 (305)",
+    "build": "Regression Build 1.72.1 (318)",
     "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
