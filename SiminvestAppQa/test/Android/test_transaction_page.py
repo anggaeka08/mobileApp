9bb78c1f49a0e749e1a14627ec1d606f257ba5a6
Arjun <arjunsinha21@gmail.com>
2023-02-08 12:57:02 +0530

    SQ-1272 Automate test cases for order details of gtc list

--- SiminvestAppQa/test/Android/test_transaction_page.py
+++ SiminvestAppQa/test/Android/test_transaction_page.py
@@ -113,6 +113,26 @@ class Transaction_test(Transaction):
             self.execute_script("lambda-status=failed")
             pytest.fail(E.msg, pytrace=True)
 
+    @pytest.mark.order_details_of_gtc_list
+    @pytest.mark.Android
+    @pytest.mark.Transaction
+    @allure.story("F-8:Transaction Page")
+    def test_ui_for_order_details_of_gtc_list(self):
+        try:
+            self.execute_script('lambda-name=test_ui_for_order_details_of_gtc_list')
+            self.open_trans_page_with_reg_user(user_data['reg_no'])
+            self.verify_order_details_of_gtc_list()
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_ui_for_order_details_of_gtc_list', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_ui_for_order_details_of_gtc_list', 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
+
+
     # Cover all 5 test cases in single test
     @pytest.mark.T_SMMA_001_to_005
     @pytest.mark.Android
