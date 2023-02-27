28284ea960d1ab89ffce69315952e08ab9383a60
Arjun <arjunsinha21@gmail.com>
2023-02-27 16:38:04 +0530

    SQ-1294 added cases for funtional feature of History detail page

--- SiminvestAppQa/test/Android/test_transaction_page.py
+++ SiminvestAppQa/test/Android/test_transaction_page.py
@@ -289,6 +289,27 @@ class Transaction_test(Transaction):
             self.execute_script("lambda-status=failed")
             pytest.fail(E.msg, pytrace=True)
 
+    @pytest.mark.functional_feature_of_history_detail_page
+    @pytest.mark.Android
+    @pytest.mark.Transaction
+    @allure.story("F-8:Transaction Page")
+    def test_functional_feature_of_history_detail_page(self):
+        try:
+            self.execute_script('lambda-name=test_functional_feature_of_history_detail_page')
+            self.open_trans_page_with_reg_user(user_data['reg_no'])
+            self.verify_functional_features_of_history_detail_page()
+            self.execute_script("lambda-status=passed")
+        except AssertionError as E:
+            self.save_screenshot('test_functional_feature_of_history_detail_page',
+                                 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.__str__(), pytrace=True)
+        except NoSuchElementException as E:
+            self.save_screenshot('test_functional_feature_of_history_detail_page',
+                                 'SiminvestAppQa/src/data/ScreenShots')
+            self.execute_script("lambda-status=failed")
+            pytest.fail(E.msg, pytrace=True)
+
     # Cover all 5 test cases in single test
     @pytest.mark.T_SMMA_001_to_005
     @pytest.mark.Android
