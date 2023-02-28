fdf03d2ddcbaa8250fe6024d4ee27aa84b0555b4
Pawan Kumar <pawan@binmile.com>
2023-02-28 16:53:09 +0530

    SQ-1301 Update test cases for build 1.76.4

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -4,8 +4,8 @@ cp = [
     "platformName": "Android",
     "deviceName": "Google Pixel 4",
     "platformVersion": "10",
-    "build": "Regression 1.76.4 (348)",
-    "name": "Regression Test Cases",
+    "build": "Testing 1.76.4 (348)",
+    "name": "Testing Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
@@ -15,8 +15,8 @@ cp = [
     "platformName": "Android",
     "deviceName": "Google Pixel 3",
     "platformVersion": "9",
-    "build": "Regression 1.76.4 (348)",
-    "name": "Regression Test Cases",
+    "build": "Testing 1.76.4 (348)",
+    "name": "Testing Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
