a934304772de809cc93b5ed8357b2c749d69a536
Pawan Kumar <pawan@binmile.com>
2023-03-16 16:29:17 +0530

    Update version of app

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -1,21 +1,21 @@
 cp = [
 {
-    "app":"lt://APP1016046941678952448079291",
+    "app":"lt://APP1016046941678961594739703",
     "platformName": "Android",
     "deviceName": "Google Pixel 3",
     "platformVersion": "9",
-    "build": "Regression 1.77.1 (359)",
+    "build": "Regression 1.77.1 (361)",
     "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
     },
 {
-    "app":"lt://APP1016046941678952448079291",
+    "app":"lt://APP1016046941678961594739703",
     "platformName": "Android",
     "deviceName": "Google Pixel 3",
     "platformVersion": "9",
-    "build": "Regression 1.77.1 (359)",
+    "build": "Regression 1.77.1 (361)",
     "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
