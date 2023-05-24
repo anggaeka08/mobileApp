4c72bdcfe0130e3bff9a31b4e593c9ca3839c08a
Pawan Kumar <pawan@binmile.com>
2023-05-24 20:14:58 +0530

    SIM-217 Update build details and portfolio section

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -13,12 +13,13 @@ cp = [
 {
     #"app":"lt://APP1016046941680173474501548",
    # "app":"lt://APP10160591891683016062192803",
-    "app":"lt://APP10160591891684303692753231",
+    #"app":"lt://APP10160591891684303692753231",
+    "app":"lt://APP1016045801684936235248413", #1.82.4(403)
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
-    "build": "Testing 1.82.1",
-    "name": "Testing Test Cases",
+    "build": "Regression 1.82.4(403)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
