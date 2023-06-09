e0ff33fac3de655bcfebc6332e8d307144d16da3
Pawan Kumar <pawan@binmile.com>
2023-06-09 10:42:06 +0530

    SIM-244 Add new build for execution

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -1,11 +1,11 @@
 cp = [
 {
-    "app":"lt://APP1016045801684936235248413",
+    "app":"lt://APP10160631101686282909016947", #1.83.1(420)
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
-    "build": "Testing 1.82.4(403)",
-    "name": "Testing Test Cases",
+    "build": "Regression 1.83.1(420)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
@@ -14,12 +14,13 @@ cp = [
     #"app":"lt://APP1016046941680173474501548",
    # "app":"lt://APP10160591891683016062192803",
     #"app":"lt://APP10160591891684303692753231",
-    "app":"lt://APP1016045801684936235248413", #1.82.4(403)
+    #"app":"lt://APP1016045801684936235248413", #1.82.4(403)
+    "app":"lt://APP10160631101686282909016947", #1.83.1(420)
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
-    "build": "Testing 1.82.4(403)",
-    "name": "Testing Test Cases",
+    "build": "Regression 1.83.1(420)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
