dde001d4dc113dd95b4a2323ded55c2852015417
Pawan Kumar <pawan@binmile.com>
2023-07-07 18:02:24 +0530

    SIM-333 Add index page execution in pipeline

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -4,8 +4,8 @@ cp = [
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
-    "build": "Testing 1.82.4(403)",
-    "name": "Testing Test Cases",
+    "build": "Regression 1.85.3 (505)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
@@ -15,12 +15,13 @@ cp = [
    # "app":"lt://APP10160591891683016062192803",
     #"app":"lt://APP10160591891684303692753231",
     #"app":"lt://APP1016045801684936235248413", #1.82.4(403)
-    "app":"lt://APP10160631101686282909016947", #1.83.1(420)
+    #"app":"lt://APP10160631101686282909016947", #1.83.1(420)
+    "app":"lt://APP1016033751688733095284962", #1.85.3 (505)
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
-    "build": "Testing 1.83.1(420)",
-    "name": "Testing Test Cases",
+    "build": "Regression 1.85.3 (505)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
