1424e630fac14beecde03af37bb69494f9493208
Pawan Kumar <pawan@binmile.com>
2023-08-04 16:10:30 +0530

    SIM-478 Update cap file accroding to latest build

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -17,12 +17,13 @@ cp = [
     #"app":"lt://APP1016045801684936235248413", #1.82.4(403)
     #"app":"lt://APP10160631101686282909016947", #1.83.1(420)
     #"app":"lt://APP1016033751688733095284962", #1.85.3 (505)
-    "app":"lt://APP1016033751689940358096184", #1.86.3 (517)
+   # "app":"lt://APP1016033751689940358096184", #1.86.3 (517)
+    "app":"lt://APP10160361191691145545430358", #1.87.3 (535)
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
-    "build": "Testing 1.86.3 (517)",
-    "name": "Testing Test Cases",
+    "build": "Regression 1.87.3 (535)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
