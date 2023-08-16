d2038b12d96bf2018ad3723540d7c012a9af489f
Pawan Kumar <pawan@binmile.com>
2023-08-16 18:06:34 +0530

    SIM-499 update cap and bitbucket file for latest build

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -1,11 +1,11 @@
 cp = [
 {
-    "app":"lt://APP1016042571691477927836795",
+    "app":"lt://APP1016039551691997859373818",
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
-    "build": "Testing 1.85.3 (505)",
-    "name": "Testing Test Cases",
+    "build": "Regression 1.88.3 (547)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
@@ -20,12 +20,13 @@ cp = [
    # "app":"lt://APP1016033751689940358096184", #1.86.3 (517)
     #"app":"lt://APP10160492501691380402841630", #1.87.3 (535)
    # "app":"lt://APP10160571901691402097003235", #1.87.4 (538)
-    "app":"lt://APP1016042571691477927836795", #1.87.5 (540)
+    #"app":"lt://APP1016042571691477927836795", #1.87.5 (540)
+    "app":"lt://APP1016039551691997859373818", #1.88.3 (547)
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
-    "build": "Testing 1.87.5 (540)",
-    "name": "Testing Test Cases",
+    "build": "Regression 1.88.3 (547)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
     "user": "pawan@binmile.com",
     "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
