0e26fda9bdb4bb56b0fbb7fda51a35999cfb8550
Djoko Utomo <djoko.utomo@sinarmassekuritas.co.id>
2023-09-12 17:54:41 +0700

    add test case

--- SiminvestAppQa/src/cap_files/lt_cap_file.py
+++ SiminvestAppQa/src/cap_files/lt_cap_file.py
@@ -1,14 +1,14 @@
 cp = [
 {
-    "app":"lt://APP1016039551691997859373818",
+    "app":"lt://APP1016039961692844102523584",
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
     "build": "Testing 1.88.3 (547)",
     "name": "Testing Test Cases",
     "isRealMobile": True,
-    "user": "pawan@binmile.com",
-    "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
+    "user": "djoko.utomo@sinarmassekuritas.co.id",
+    "accessKey": "q5QNqu2BGiYl7ncqTxKMFZTTPFVHxElYIgRbhtvZHqTTgOxQSi"
     },
 {
     #"app":"lt://APP1016046941680173474501548",
@@ -21,15 +21,16 @@ cp = [
     #"app":"lt://APP10160492501691380402841630", #1.87.3 (535)
    # "app":"lt://APP10160571901691402097003235", #1.87.4 (538)
     #"app":"lt://APP1016042571691477927836795", #1.87.5 (540)
-    "app":"lt://APP1016039551691997859373818", #1.88.3 (547)
+    #"app":"lt://APP1016039551691997859373818", #1.88.3 (547)
+    "app":"lt://APP10160391251694514606624635", #1.90.0 (566)
     "platformName": "Android",
     "deviceName": "Pixel 4",
     "platformVersion": "10",
-    "build": "Testing 1.88.3 (547)",
-    "name": "Testing Test Cases",
+    "build": "Regression 1.90.0 (566)",
+    "name": "Regression Test Cases",
     "isRealMobile": True,
-    "user": "pawan@binmile.com",
-    "accessKey": "nrVDKFpgRayhZV7Y9KGQh6J7MuJ4M4rWbNZV4Y0eYZzmJ3sTlj"
+    "user": "djoko.utomo@sinarmassekuritas.co.id",
+    "accessKey": "q5QNqu2BGiYl7ncqTxKMFZTTPFVHxElYIgRbhtvZHqTTgOxQSi"
     }
 
 ]
